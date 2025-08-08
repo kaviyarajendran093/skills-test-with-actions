2f9217bd7d433cb6ef1772125345414c2e15263b
kaviya <kaviyaraj1193@gmail.com>
2025-08-08 20:05:40 +0000

    fix the 2nd error

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -67,8 +67,8 @@ def test_get_nth_fibonacci_ten():
 
      # Assert
      assert result == 55
-
-def test_area_of_circle_negative_radius(self):
+class TestCalculations:
+ def test_area_of_circle_negative_radius(self):
    """Test with a negative radius to raise ValueError."""
    # Arrange
    radius = -1
@@ -77,7 +77,7 @@ def test_area_of_circle_negative_radius(self):
    with self.assertRaises(ValueError):
       area_of_circle(radius)
 
-def test_get_nth_fibonacci_negative(self):
+ def test_get_nth_fibonacci_negative(self):
    """Test with a negative number to raise ValueError."""
    # Arrange
    n = -1
